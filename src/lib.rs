5a38b0e896914ff008e72c9aee6dbaa69449bc9c
Andrew Sonin <sonin.cel@yandex.ru>
2021-08-03 15:38:35 +0300

    Reorganize prelude

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,23 @@ pub mod input;
 
 pub mod prelude {
     pub use crate::exchange::Exchange;
-    pub use crate::input::*;
-    pub use crate::message::*;
-    pub use crate::trader::*;
-    pub use crate::types::*;
+    pub use crate::input;
+    pub use crate::input::{cli::ArgumentParser, cli::Clap, inline::StaticInput};
+    pub use crate::message::{
+        CancellationReason,
+        DiscardingReason,
+        ExchangeReply,
+        InabilityToCancelReason,
+        TraderRequest,
+    };
+    pub use crate::trader::{examples, Trader};
+    pub use crate::types::{
+        Duration,
+        NonZeroU64,
+        OrderDirection,
+        OrderID,
+        OrderSize,
+        Price,
+        Timestamp,
+    };
 }

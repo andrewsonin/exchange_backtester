79c340f37b6d16ec1b61f2304e61441dc79357df
Andrew Sonin <sonin.cel@yandex.ru>
2021-08-03 15:02:39 +0300

    Change input::default privacy rules

--- src/input/default.rs
+++ src/input/default.rs
@@ -1,8 +1,8 @@
-pub(crate) const ORDER_TIMESTAMP_COLNAME: &str = "Timestamp";
-pub(crate) const ORDER_ID_COLNAME: &str = "ORDER_ID";
-pub(crate) const ORDER_PRICE_COLNAME: &str = "ORDER_PRICE";
-pub(crate) const ORDER_SIZE_COLNAME: &str = "ORDER_SIZE";
-pub(crate) const ORDER_BS_FLAG_COLNAME: &str = "BUY_SELL_FLAG";
-pub(crate) const DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S%.f";
-pub(crate) const CSV_SEP: &str = ",";
-pub(crate) const PRICE_STEP: &str = "0.0025";
+pub const ORDER_TIMESTAMP_COLNAME: &str = "Timestamp";
+pub const ORDER_ID_COLNAME: &str = "ORDER_ID";
+pub const ORDER_PRICE_COLNAME: &str = "ORDER_PRICE";
+pub const ORDER_SIZE_COLNAME: &str = "ORDER_SIZE";
+pub const ORDER_BS_FLAG_COLNAME: &str = "BUY_SELL_FLAG";
+pub const DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S%.f";
+pub const CSV_SEP: &str = ",";
+pub const PRICE_STEP: &str = "0.0025";

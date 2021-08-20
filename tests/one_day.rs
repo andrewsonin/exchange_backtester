777e619a4818a011f940adce2532555e284d086d
Andrew Sonin <sonin.cel@yandex.ru>
2021-08-20 16:49:10 +0300

    Add one_day test assertion

--- tests/one_day.rs
+++ tests/one_day.rs
@@ -5,15 +5,16 @@ use std::{
     io::{BufWriter, Write},
     path::Path,
 };
+use std::fs::read_to_string;
 
 use exchange_backtester::prelude::*;
 
-struct CustomTrader {
+struct CustomTrader<'a> {
     price_step: f64,
-    file_to_write: BufWriter<File>,
+    file_to_write: &'a mut BufWriter<File>,
 }
 
-impl HandleSubscriptionUpdates for CustomTrader {
+impl HandleSubscriptionUpdates for CustomTrader<'_> {
     fn handle_order_book_snapshot(&mut self,
                                   exchange_ts: Timestamp,
                                   _: Timestamp,
@@ -39,7 +40,7 @@ impl HandleSubscriptionUpdates for CustomTrader {
     }
 }
 
-impl const Trader for CustomTrader {
+impl const Trader for CustomTrader<'_> {
     fn exchange_to_trader_latency(_: &mut StdRng, _: Timestamp) -> u64 { 0 }
     fn trader_to_exchange_latency(_: &mut StdRng, _: Timestamp) -> u64 { 0 }
     fn handle_exchange_reply(&mut self, _: Timestamp, _: Timestamp, _: ExchangeReply) -> Vec<TraderRequest> {
@@ -65,7 +66,7 @@ fn main() {
 
     let mut trader = CustomTrader {
         price_step: input.get_price_step(),
-        file_to_write: buffer,
+        file_to_write: &mut buffer,
     };
 
     let is_trading_time = |timestamp: Timestamp| {
@@ -82,5 +83,28 @@ fn main() {
         is_trading_time,
     )
         .ob_level_subscription_depth(lags::constant::ONE_HOUR, 1)
-        .run_trades()
+        .run_trades();
+
+    drop(buffer);
+    let file_content = read_to_string(path.join("output.csv")).unwrap();
+    assert_eq!(
+        file_content,
+        "Timestamp,MidPrice\n\
+        2021-06-01 08:00:00.039,73.32250\n\
+        2021-06-01 09:00:00.039,73.31750\n\
+        2021-06-01 10:00:00.039,73.18250\n\
+        2021-06-01 11:00:00.039,73.27000\n\
+        2021-06-01 12:00:00.039,73.33625\n\
+        2021-06-01 13:00:00.039,73.48625\n\
+        2021-06-01 14:00:00.039,73.44875\n\
+        2021-06-01 15:00:00.039,73.54250\n\
+        2021-06-01 16:00:00.039,73.59750\n\
+        2021-06-01 17:00:00.039,73.45500\n\
+        2021-06-01 18:00:00.039,73.48875\n\
+        2021-06-01 19:00:00.039,73.45250\n\
+        2021-06-01 20:00:00.039,73.53500\n\
+        2021-06-01 21:00:00.039,73.55375\n\
+        2021-06-01 22:00:00.039,73.49000\n\
+        2021-06-01 23:00:00.039,73.50500\n"
+    )
 }
